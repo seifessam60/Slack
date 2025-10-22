8493b1e1783b47bb619eaf695cb003c058b69ad2
Seif Essam <seifessam60@gmail.com>
2025-10-23 01:26:59 +0300

    db connection

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,4 +4,10 @@ export const ENV = {
   PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   NODE_ENV: process.env.NODE_ENV,
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  STREAM_API_KEY: process.env.STREAM_API_KEY,
+  STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+  SENTRY_DSN: process.env.SENTRY_DSN,
+  INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
 };
