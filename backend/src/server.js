87fe1bc89283221cfa97d4090967a5775faa2e3d
Seif Essam <seifessam60@gmail.com>
2025-10-25 20:54:23 +0300

    stream & sentry added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,27 @@
+import "../instrument.mjs";
 import express from "express";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
 import { functions, inngest } from "./config/innjest.js";
 import { serve } from "inngest/express";
+import chatRoutes from "./routes/chatRoutes.js";
+import * as Sentry from "@sentry/node";
 
 const app = express();
 app.use(express.json());
 app.use(clerkMiddleware());
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
+app.get("/debug-sentry", function main(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/api/inngest", serve({ client: inngest, functions }));
+app.use("/api/chat", chatRoutes);
+Sentry.setupExpressErrorHandler(app);
 
 const startServer = async () => {
   try {
