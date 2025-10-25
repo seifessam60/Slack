5dd0739220af4328a0bbf1134633f52b3c83d98d
Seif Essam <seifessam60@gmail.com>
2025-10-25 21:47:50 +0300

    auth page added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 
@@ -14,7 +15,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
   </StrictMode>
 );
