e2962a29eff4f1aa10700108375d413be53a9c2e
Seif Essam <seifessam60@gmail.com>
2025-10-28 02:05:27 +0300

    invite modal added

--- backend/src/config/innjest.js
+++ backend/src/config/innjest.js
@@ -1,7 +1,11 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import { User } from "../models/User.js";
-import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+import {
+  addUserToPublicChannels,
+  deleteStreamUser,
+  upsertStreamUser,
+} from "./stream.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "slack" });
@@ -27,6 +31,7 @@ const syncUser = inngest.createFunction(
       image: newUser.image,
       email: newUser.email,
     });
+    await addUserToPublicChannels(newUser.clerkId.toString());
   }
 );
 const deleteUserFromDB = inngest.createFunction(
